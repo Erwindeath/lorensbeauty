b46083a402dc8fdfacc54542445371f0502bef2d
ErwinAvilaSG <33210443+Erwindeath@users.noreply.github.com>
2025-02-06 09:22:04 -0500

    Sistema para peliquería

---
+++ android/app/src/main/kotlin/com/example/lorensbeauty/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.lorensbeauty
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/controller/auth_controller.dart
@@ -0,0 +1,79 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/foundation.dart' show kIsWeb;
+import 'package:flutter/material.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+
+class Authentication {
+  static Future<User?> signInWithGoogle({required BuildContext context}) async {
+    FirebaseAuth auth = FirebaseAuth.instance;
+    User? user;
+
+    if (kIsWeb) {
+      GoogleAuthProvider authProvider = GoogleAuthProvider();
+
+      try {
+        final UserCredential userCredential =
+            await auth.signInWithPopup(authProvider);
+        print(userCredential.user);
+        user = userCredential.user;
+      } catch (e) {
+        print(e);
+      }
+    } else {
+      print("****************AQUI************************");
+      final GoogleSignIn googleSignIn = GoogleSignIn(
+        scopes: ["profile", "email"],
+      );
+      try {
+        final GoogleSignInAccount? googleUser = await googleSignIn.signIn();
+        if (googleUser != null) {
+          print("Nombre: ${googleUser.displayName}");
+          print("Correo: ${googleUser.email}");
+        } else {
+          print("El usuario canceló el inicio de sesión.");
+        }
+      } catch (e) {
+        print("Error durante el inicio de sesión: $e");
+      }
+      final GoogleSignInAccount? googleSignInAccount =
+          await googleSignIn.signIn();
+      print("asdasdasdasd");
+      print(googleSignInAccount);
+      if (googleSignInAccount != null) {
+        final GoogleSignInAuthentication googleSignInAuthentication =
+            await googleSignInAccount.authentication;
+
+        final AuthCredential credential = GoogleAuthProvider.credential(
+          accessToken: googleSignInAuthentication.accessToken,
+          idToken: googleSignInAuthentication.idToken,
+        );
+
+        try {
+          print("LLEGANDO AQUIASDASDASDASD");
+          final UserCredential userCredential =
+              await auth.signInWithCredential(credential);
+
+          user = userCredential.user;
+        } on FirebaseAuthException catch (e) {
+          if (e.code == 'account-exists-with-different-credential') {
+          } else if (e.code == 'invalid-credential') {}
+        }
+      }
+    }
+    // print(user);
+    return user;
+  }
+
+  static Future<void> signOut({required BuildContext context}) async {
+    final GoogleSignIn googleSignIn = GoogleSignIn();
+
+    try {
+      if (!kIsWeb) {
+        await googleSignIn.signOut();
+      }
+      await FirebaseAuth.instance.signOut();
+    } catch (e) {
+      print(e);
+    }
+  }
+}
